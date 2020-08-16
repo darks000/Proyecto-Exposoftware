0b809718bcc09baae73e0c02418c0be1a02b14c1
Dario Ramos <darks1497@gmail.com>
2020-08-16 16:04:05 -0500

    Prueba2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,18 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)
+  },
+  {
+    path: 'estudiantes',
+    loadChildren: () => import('./estudiantes/estudiantes.module').then( m => m.EstudiantesPageModule)
+  },
+  {
+    path: 'proyecto',
+    loadChildren: () => import('./proyecto/proyecto.module').then( m => m.ProyectoPageModule)
   }
 ];
 @NgModule({
